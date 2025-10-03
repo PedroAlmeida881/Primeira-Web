b0e5fc7ac5d335b2c66c616a0d6e9998c20213ef
PedroAlmeida881 <pedroalopes0612@gmail.com>
2025-10-03 10:46:35 -0300

    Add files via upload

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,22 +1,41 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "quiz-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  ".//icone-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('app-saudacao-cache').then(cache =>  {
-      return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png',
-        'manha.jpg',
-        'tarde.jpg',
-        'noite.jpg',
-        'madrugada.jpg'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
     })
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request)) 
-  );
-    });
